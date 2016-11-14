443794d1f0170db30ca1479bbeb0aee547ee7365
Kommirisetty,Prabhath Teja <s525730@ch140030m.nwmissouri.edu>
2016-11-14 13:24:24 -0600

    Initial Commit

---
+++ Git-VPPK/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-VPPK
+//
+//  Created by Kommirisetty,Prabhath Teja on 11/14/16.
+//  Copyright © 2016 Northwest missouri state university. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
